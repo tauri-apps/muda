5fbe39e995f96139711162ff39848b6b9373f728
amrbashir <amr.bashir2015@gmail.com>
2023-07-26 03:37:19 +0300

    chore: fix wry example context menu closes immediately

--- examples/wry.rs
+++ examples/wry.rs
@@ -166,7 +166,15 @@ fn main() -> wry::Result<()> {
         <script>
             window.addEventListener('contextmenu', (e) => {
                 e.preventDefault();
-                window.ipc.postMessage(`showContextMenu:${e.clientX},${e.clientY}`);
+                // if e.button is -1 on chromuim or e.buttons is 0 (as a fallback on webkit2gtk) then this event was fired by keyboard
+                if (e.button === -1 || e.buttons === 0) {
+                    window.ipc.postMessage(`showContextMenu:${e.clientX},${e.clientY}`);
+                }
+            })
+            window.addEventListener('mouseup', (e) => {
+                if (e.button === 2) {
+                    window.ipc.postMessage(`showContextMenu:${e.clientX},${e.clientY}`);
+                }
             })
         </script>
     </body>
