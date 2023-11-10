a1bcc3ef06ffcc96e5076ebfcba0f4a6a89cbf17
Morgan Hill <a@morganrhill.com>
2023-11-10 16:21:08 +0100

    Add running test with miri to CI
    For T1 Rust targets Miri is able to check for several forms of undefined
    behavior that rustc can not.

--- src/items/mod.rs
+++ src/items/mod.rs
@@ -19,6 +19,7 @@ mod test {
     use crate::{CheckMenuItem, IconMenuItem, MenuId, MenuItem, PredefinedMenuItem, Submenu};
 
     #[test]
+    #[cfg_attr(all(miri, not(target_os = "linux")), ignore)]
     fn it_returns_same_id() {
         let id = MenuId::new("1");
         assert_eq!(id, MenuItem::with_id(id.clone(), "", true, None).id());
@@ -34,6 +35,7 @@ mod test {
     }
 
     #[test]
+    #[cfg_attr(all(miri, not(target_os = "linux")), ignore)]
     fn test_convert_from_id_and_into_id() {
         let id = "TEST ID";
         let expected = MenuId(id.to_string());
