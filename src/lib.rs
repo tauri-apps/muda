5e8e3e32ad746c04f697d78df7c286f0f4a6112e
amrbashir <amr.bashir2015@gmail.com>
2024-03-27 01:05:48 +0200

    fix macOS and Linux builds

--- src/lib.rs
+++ src/lib.rs
@@ -100,7 +100,7 @@
 //! # #[cfg(target_os = "macos")]
 //! # let nsview = 0 as *mut objc::runtime::Object;
 //! // --snip--
-//! let position = muda::PhysicalPosition { x: 100., y: 120. };
+//! let position = muda::dpi::PhysicalPosition { x: 100., y: 120. };
 //! #[cfg(target_os = "windows")]
 //! menu.show_context_menu_for_hwnd(window_hwnd, Some(position.into()));
 //! #[cfg(target_os = "linux")]
@@ -327,7 +327,7 @@ pub trait ContextMenu {
     ///
     /// - `position` is relative to the window top-left corner, if `None`, the cursor position is used.
     #[cfg(target_os = "linux")]
-    fn show_context_menu_for_gtk_window(&self, w: &gtk::Window, position: Option<Position>);
+    fn show_context_menu_for_gtk_window(&self, w: &gtk::Window, position: Option<dpi::Position>);
 
     /// Get the underlying gtk menu reserved for context menus.
     #[cfg(target_os = "linux")]
@@ -337,7 +337,7 @@ pub trait ContextMenu {
     ///
     /// - `position` is relative to the window top-left corner, if `None`, the cursor position is used.
     #[cfg(target_os = "macos")]
-    fn show_context_menu_for_nsview(&self, view: cocoa::base::id, position: Option<Position>);
+    fn show_context_menu_for_nsview(&self, view: cocoa::base::id, position: Option<dpi::Position>);
 
     /// Get the underlying NSMenu reserved for context menus.
     #[cfg(target_os = "macos")]
