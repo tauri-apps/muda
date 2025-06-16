ed7fe4d6ab0ab8865fdef86073cada2b15ed1443
Pavel Kuzmin <Virus191288@gmail.com>
2025-06-16 20:40:23 +0500

    style(gtk): format long lines in `mod.rs` for better readability
    Refactor long lines in `src/platform_impl/gtk/mod.rs` to improve
    readability. This change enhances code maintainability without altering
    functionality.

--- src/platform_impl/gtk/mod.rs
+++ src/platform_impl/gtk/mod.rs
@@ -143,7 +143,8 @@ impl Menu {
         return_if_item_not_supported!(item);
 
         if let (menu_id, Some(menu)) = &self.gtk_menu {
-            let gtk_item = item.make_gtk_menu_item(*menu_id, self.accel_group.as_ref(), true, false)?;
+            let gtk_item = 
+                item.make_gtk_menu_item(*menu_id, self.accel_group.as_ref(), true, false)?;
             menu.append(&gtk_item);
             gtk_item.show();
         }
@@ -831,7 +832,8 @@ impl MenuChild {
         return_if_item_not_supported!(item);
 
         if let Some((menu_id, Some(menu))) = self.gtk_menu.as_ref() {
-            let gtk_item = item.make_gtk_menu_item(*menu_id, self.accel_group.as_ref(), true, false)?;
+            let gtk_item = 
+                item.make_gtk_menu_item(*menu_id, self.accel_group.as_ref(), true, false)?;
             menu.append(&gtk_item);
             gtk_item.show();
         }
