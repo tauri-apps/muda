4adda8edd921662855d99bcfdde87168485f3e47
amrbashir <github@amrbashir.me>
2026-07-22 06:13:32 +0300

    remove unused from_gtk_mnemonic function and related tests

--- src/platform_impl/gtk/mnemonic.rs
+++ src/platform_impl/gtk/mnemonic.rs
@@ -20,27 +20,9 @@ pub fn to_gtk_mnemonic<S: AsRef<str>>(string: S) -> String {
         .replace("[~~]", "&")
 }
 
-/// Converts from gtk mnemonic to muda mnemonic
-///
-/// gtk uses underline (_) for mnemonic
-/// and two underlines (__) to escape it into a single underline
-/// while we use (&) and (&&), so we have to do a few conversions
-pub fn from_gtk_mnemonic<S: AsRef<str>>(string: S) -> String {
-    string
-        .as_ref()
-        // transform escaped & to unescaped &&
-        .replace("&", "&&")
-        // perserve __
-        .replace("__", "[~~]")
-        // transfrom _ -> &
-        .replace('_', "&")
-        // revert back __ to unescaped _
-        .replace("[~~]", "_")
-}
-
 #[cfg(test)]
 mod tests {
-    use crate::platform_impl::platform::mnemonic::{from_gtk_mnemonic, to_gtk_mnemonic};
+    use super::*;
 
     #[test]
     fn it_converts() {
@@ -49,23 +31,5 @@ mod tests {
         assert_eq!(to_gtk_mnemonic("H&&&ello"), "H&_ello");
         assert_eq!(to_gtk_mnemonic("H_ello"), "H__ello");
         assert_eq!(to_gtk_mnemonic("H__ello"), "H____ello");
-    }
-
-    #[test]
-    fn it_converts_back() {
-        let str = "H&ello";
-        assert_eq!(from_gtk_mnemonic(to_gtk_mnemonic(str)), str);
-
-        let str = "H&&ello";
-        assert_eq!(from_gtk_mnemonic(to_gtk_mnemonic(str)), str);
-
-        let str = "H&&&ello";
-        assert_eq!(from_gtk_mnemonic(to_gtk_mnemonic(str)), str);
-
-        let str = "H_ello";
-        assert_eq!(from_gtk_mnemonic(to_gtk_mnemonic(str)), str);
-
-        let str = "H__ello";
-        assert_eq!(from_gtk_mnemonic(to_gtk_mnemonic(str)), str);
     }
 }
