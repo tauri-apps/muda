171547e22657f9164f413610fd2984b130c2c5a5
amrbashir <github@amrbashir.me>
2026-08-31 19:14:08 +0300

    Fix SPDX license identifier in multiple files

--- src/items/predefined.rs
+++ src/items/predefined.rs
@@ -1,5 +1,5 @@
 // Copyright 2022-2022 Tauri Programme within The Commons Conservancy
-// SPDX-License-Identifier: Apache-2.inner
+// SPDX-License-Identifier: Apache-2.0
 // SPDX-License-Identifier: MIT
 
 use std::{cell::RefCell, mem, rc::Rc};
