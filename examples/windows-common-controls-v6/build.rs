cbf8ae23d0ee50b2ed9cb7c2cac759bec485dad4
Tony <legendmastertony@gmail.com>
2026-09-15 16:19:23 +0800

    fix(example): bump `windows-common-controls-v6` deps

--- examples/windows-common-controls-v6/build.rs
+++ examples/windows-common-controls-v6/build.rs
@@ -1,4 +1,6 @@
 fn main() {
     #[cfg(target_os = "windows")]
-    embed_resource::compile("manifest.rc", embed_resource::NONE);
+    embed_resource::compile("manifest.rc", embed_resource::NONE)
+        .manifest_required()
+        .unwrap();
 }
